7619e5775c5136f7c68def65b3316142b294d25e
carlafeliu <caaarla03@gmail.com>
2022-11-07 18:14:27 +0100

    prova4

---
+++ chemproptest.sh
@@ -0,0 +1,4 @@
+#Instruccions
+#wget: instal·lar un fitxer de qualsevol lloc (pàgina web). Si des de python poses !wget, se'n va a la whell i ho executa). Et permet convinar instruccions de python i de shell
+#chemprop_train --data_path /Users/carlafeliu/Documentos/Master/TFM/chemprop/data/tox21.csv --dataset_type classification --save_dir tox21_checkpoint
+#nohup chemprop_interpret --data_path data/tox21.csv --checkpoint_dir /Users/carlafeliu/Documentos/Master/TFM/chemprop/tox21_checkpoint/fold_0/ --property_id 1 > interpret.log & #nohub i & al final serveix per enviar-ho a fer a radera
