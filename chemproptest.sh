10580ea237bca0b2f82c9d74767fb321e2917abb
carlafeliu <caaarla03@gmail.com>
2022-11-21 17:38:02 +0100

    Actualització script

--- chemproptest.sh
+++ chemproptest.sh
@@ -1,12 +1,18 @@
 conda activate tfm
+#descarrega els fitxers
+#HOME es una variable de la shell, t'ho guarda al inici 
 
+CHEMPROP_RES=$HOME/scratch #aquí li dones una variable amb un path
+mkdir $CHEMPROP_RES #Crees el directori de la variable 
+wget https://github.com/chemprop/chemprop/blob/master/data.tar.gz -O $CHEMPROP_RES/data.tar.gz
+tar zxvf $CHEMPROP_RES/data.tar.gz #descomprimir el tar, això crearà la carpeta data
 
 #Train
-python train.py  --data_path /Users/carlafeliu/Docs/Master/TFM/chemprop/data/tox21.csv --dataset_type classification --save_dir tox21_checkpoint
+python train.py  --data_path $CHEMPROP_RES/data/tox21.csv --dataset_type classification --save_dir $CHEMPROP_RES/tox21_checkpoint
 
 #predicting
-chemprop_predict --test_path data/tox21.csv --checkpoint_dir /Users/carlafeliu/Docs/Master/TFM/chemprop/tox21_checkpoint --preds_path tox21_preds.csv
+chemprop_predict --test_path $CHEMPROP_RES/data/tox21.csv --checkpoint_dir $CHEMPROP_RES/tox21_checkpoint --preds_path $CHEMPROP_RES/tox21_preds.csv
 
 
 #Interpret
-nohub chemprop_interpret --data_path data/tox21.csv --checkpoint_dir /Users/carlafeliu/Docs/Master/TFM/chemprop/tox21_checkpoint/fold_0/ --property_id 1 > interpret.log &
+nohub chemprop_interpret --data_path $CHEMPROP_RES/data/tox21.csv --checkpoint_dir $CHEMPROP_RES/tox21_checkpoint/fold_0/ --property_id 1 > $CHEMPROP_RES/interpret.log &
