42727dd0eb6fe764d354157c122629c226062df8
carlafeliu <caaarla03@gmail.com>
2022-11-21 17:00:45 +0100

    test

---
+++ Introducció cysteine inhibitors.md
@@ -0,0 +1,19 @@
+# Chemprop execute
+```{code}
+conda activate tfm
+```
+
+# Covalent inhibitors
+Covalent inhibidors are a class of small molecule compounds that can covalently bind to specific target protein inhibiting their biological funcions. 
+More of the most important adventages of covalent inhibidors, is their high binding affinity to target proteins, is much more stable than non-covalent inhibitors and prolonging the duration of action. (mirar més avantatges)
+
+
+
+---
+
+
+
+A covalent labeling is achieved when a leaving group from an electro-deficient aromatic ring is replaced by a nucleophile, such as cysteine thiol
+
+
+
--- chemproptest.sh
+++ chemproptest.sh
@@ -1,4 +1,12 @@
-#Instruccions
-#wget: instal·lar un fitxer de qualsevol lloc (pàgina web). Si des de python poses !wget, se'n va a la whell i ho executa). Et permet convinar instruccions de python i de shell
-#chemprop_train --data_path /Users/carlafeliu/Documentos/Master/TFM/chemprop/data/tox21.csv --dataset_type classification --save_dir tox21_checkpoint
-#nohup chemprop_interpret --data_path data/tox21.csv --checkpoint_dir /Users/carlafeliu/Documentos/Master/TFM/chemprop/tox21_checkpoint/fold_0/ --property_id 1 > interpret.log & #nohub i & al final serveix per enviar-ho a fer a radera
+conda activate tfm
+
+
+#Train
+python train.py  --data_path /Users/carlafeliu/Docs/Master/TFM/chemprop/data/tox21.csv --dataset_type classification --save_dir tox21_checkpoint
+
+#predicting
+chemprop_predict --test_path data/tox21.csv --checkpoint_dir /Users/carlafeliu/Docs/Master/TFM/chemprop/tox21_checkpoint --preds_path tox21_preds.csv
+
+
+#Interpret
+nohub chemprop_interpret --data_path data/tox21.csv --checkpoint_dir /Users/carlafeliu/Docs/Master/TFM/chemprop/tox21_checkpoint/fold_0/ --property_id 1 > interpret.log &
