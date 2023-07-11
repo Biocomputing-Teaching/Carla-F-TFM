3e2f7f833d7333102bec76b210b3d823e385c5f6
carlafeliu <caaarla03@gmail.com>
2023-07-11 16:03:16 +0200

    prova chemprop

---
+++ code/chempropprova.sh
@@ -0,0 +1,3 @@
+chemprop_train --data_path /Users/carlafeliu/Docs/Master/TFM/github/TFM/code/data_chemprop/chemprop_train.csv --dataset_type regression --save_dir /Users/carlafeliu/Docs/Master/TFM/github/TFM/df_checkpoint
+#chemprop_predict --test_path /Users/carlafeliu/Docs/Master/TFM/github/TFM/data_chemprop/chemprop_train.csv --checkpoint_dir /Users/carlafeliu/Docs/Master/TFM/github/TFM/df_checkpoint --preds_path /Users/carlafeliu/Docs/Master/TFM/github/TFM/df_checkpoint/preds1.csv
+#chemprop_predict --test_path /Users/carlafeliu/Docs/Master/TFM/github/TFM/data_chemprop/chemprop_train.csv --checkpoint_path /Users/carlafeliu/Docs/Master/TFM/github/TFM/df_checkpoint/fold_0/model_0/model.pt --preds_path chemp_preds.csv
